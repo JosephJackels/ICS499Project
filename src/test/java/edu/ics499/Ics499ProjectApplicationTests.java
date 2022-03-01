f6e232f652b288e53a45a8d4b99df8937b0cb82c
leoflewis <fleonardolewis@gmail.com>
2022-03-01 16:21:10 -0600

    Played around with testing and added a method.

--- src/test/java/edu/ics499/Ics499ProjectApplicationTests.java
+++ src/test/java/edu/ics499/Ics499ProjectApplicationTests.java
@@ -1,7 +1,11 @@
 package edu.ics499;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.*;
+
+import edu.ics499.model.*;
 
 @SpringBootTest
 class Ics499ProjectApplicationTests {
@@ -10,4 +14,9 @@ class Ics499ProjectApplicationTests {
 	void contextLoads() {
 	}
 
+    public static void testDashboard() {
+        Dashboard dashboard = new Dashboard();
+        dashboard.setDashboardID(null);
+        assertEquals(dashboard.getDashboardID(), null);
+    }
 }
