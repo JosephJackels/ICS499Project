e6e53f631b688ff83d755a0bcafc39904b7b5046
leoflewis <fleonardolewis@gmail.com>
2022-03-01 17:55:10 -0600

    added more preliminary tests

--- src/test/java/edu/ics499/Ics499ProjectApplicationTests.java
+++ src/test/java/edu/ics499/Ics499ProjectApplicationTests.java
@@ -6,17 +6,33 @@ import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.*;
 
 import edu.ics499.model.*;
+import edu.ics499.model.widgets.*;
 
 @SpringBootTest
 class Ics499ProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
+	    testDashboard();
+	    testWidget();
+	    testUser();
 	}
 
-    public static void testDashboard() {
+    public void testDashboard() {
         Dashboard dashboard = new Dashboard();
         dashboard.setDashboardID(null);
         assertEquals(dashboard.getDashboardID(), null);
+    }
+
+    public void testWidget() {
+        Widget widget = new Widget();
+        widget.setWidgetID(null);
+        assertEquals(widget.getWidgetID(), null);
+    }
+
+    public void testUser() {
+        User user = new User();
+        user.setUsername("user12345");
+        assertEquals(user.getUsername(), "user12345");
     }
 }
