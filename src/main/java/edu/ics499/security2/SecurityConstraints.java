97d8183990dcb248e05dbfa7ef62ed3b05a8b3c2
Joseph Jackels <jackelsjoseph@gmail.com>
2022-03-02 08:51:08 -0600

    Security update

--- src/main/java/edu/ics499/security2/SecurityConstraints.java
+++ src/main/java/edu/ics499/security2/SecurityConstraints.java
@@ -1,9 +1,11 @@
 package edu.ics499.security2;
 
 public class SecurityConstraints {
-	public static final String SECRET = "SECRET_KEY";//change this after testing to a env variable, will have to drop tables and restart
-	public static final long EXPIRATION_TIME = 1_800_000; //15 minutes
+	public static final String SECRET = "SECRET_KEY";//change this after testing to a env variable, will have to drop tables and restart, also should be 32 chars long
+	public static final long EXPIRATION_TIME = 60_000; //1 minutes
+	public static final long REFRESH_EXPIRATION_TIME = 900_000;//2min
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
+	public static final String REFRESH_HEADER_STRING = "Refresh";
 	public static final String SIGN_UP_URL = "/users/add";
 }
