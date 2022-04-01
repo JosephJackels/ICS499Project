3f7e0785c0e785107156d144883cd251ef215d3e
leoflewis <fleonardolewis@gmail.com>
2022-04-01 14:44:54 -0500

    added the rest of the widget functionality

--- frontend/499/src/app/home/home.component.ts
+++ frontend/499/src/app/home/home.component.ts
@@ -9,8 +9,26 @@ export class HomeComponent implements OnInit {
 
   navTitle:string = 'Weather Dashboard';
   constructor() { }
+  calendar_widgets = [0];
+  weather_widgets = [0];
+  i = 0; 
 
   ngOnInit(): void {
   }
 
+  addWidget() {
+    this.calendar_widgets.push(0);
+  }
+
+  removeWidget(CalendarData: any){
+    this.calendar_widgets.pop();
+  }
+
+  addWeather() {
+    this.weather_widgets.push(0);
+  }
+
+  removeWeather(WeatherData:any){
+    this.weather_widgets.pop();
+  }
 }
