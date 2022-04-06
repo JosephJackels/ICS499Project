75e21b87f8aba2c50fd6e3cb42fe73a6811e9f42
Joseph Jackels <jackelsjoseph@gmail.com>
2022-04-06 08:40:32 -0500

    made posts work I think

--- frontend/499/src/app/aboutus/aboutus.component.ts
+++ frontend/499/src/app/aboutus/aboutus.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataServiceService } from '../service/data-service.service';
+import { Login } from '../service/login';
 import { User } from '../service/user';
 @Component({
   selector: 'app-aboutus',
@@ -8,6 +9,7 @@ import { User } from '../service/user';
 })
 export class AboutusComponent implements OnInit {
   user!: User;
+  login!: Login;
 
   constructor(private data:DataServiceService) {}
 
@@ -17,6 +19,14 @@ export class AboutusComponent implements OnInit {
   public getUser(){
     //change the 48 below to whatever id of user you want to get/have access to
     return this.data.getUser(48).subscribe({next: (value: User) => this.user = value});
+  }
+
+  public loginUser(){
+    //change vals to whoever to login
+    return this.data.loginUser('joe', 'password').subscribe({next: (value: Login) => {
+      this.login = value;
+      console.log(this.login)}
+    });
   }
 }
 
--- frontend/499/src/app/service/data-service.service.ts
+++ frontend/499/src/app/service/data-service.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 import { User } from './user'
-
+import { Login } from './login'
 @Injectable({
   providedIn: 'root'
 })
@@ -14,8 +14,32 @@ export class DataServiceService {
   constructor(private http: HttpClient) { 
   }
 
+  getAllUsers(): Observable<User>{
+    return this.http.get<User>(this.ROOT_URL + '/users/all')
+  }
+
   getUser(id: any): Observable<User>{ 
     return this.http.get<User>(this.ROOT_URL + '/users/one/' + id); 
+  }
+
+  loginUser(user: any, pass: any): Observable<Login>{
+    let options = {headers : new HttpHeaders({'Content-Type':'application/json'})};
+    let body = JSON.stringify({username: user, password: pass});
+    console.log(body);
+    return this.http.post<Login>(
+      this.ROOT_URL + '/login',
+      body,
+      options)
+  }
+
+  createUser(username: any, password: any): Observable<User>{
+    let options = {headers : new HttpHeaders({'Content-Type':'application/json'})};
+    let body = '{username: "' + username + '", password: "' + password + '"}'
+    return this.http.post<User>(
+      this.ROOT_URL + '/users/add',
+      body,
+      options
+    )
   }
 }
 
