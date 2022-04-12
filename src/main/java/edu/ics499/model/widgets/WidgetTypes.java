e12fd71776625ccf25166a354772930019ba262d
Joseph Jackels <jackelsjoseph@gmail.com>
2022-04-12 11:06:44 -0500

    tinkering wiht widget types

--- src/main/java/edu/ics499/model/widgets/WidgetTypes.java
+++ src/main/java/edu/ics499/model/widgets/WidgetTypes.java
@@ -1,6 +1,8 @@
 package edu.ics499.model.widgets;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /*
@@ -12,16 +14,25 @@ import java.util.Map;
  * 			To do so: right click on WidgetServiceImp.java => hover over RunAs, click Run Configurations. scroll over to ENvironment tab and create key.
  */
 public class WidgetTypes {
+	
+	public static final List<String> types = new ArrayList<>(List.of(
+			"currentWeather",
+			"forecastWeather",
+			"calendar",
+			"stock"));
+	
 	public static final Map<String, String> mapWidgetTypeToUrl = new HashMap<String, String>(Map.of(
 			"currentWeather", "http://api.openweathermap.org/data/2.5/weather?q=",
 			"forecastWeather", "http://api.openweathermap.org/data/2.5/forecast?q=",
 			"calendar", "",
 			"stock", "INSERT STOCK URL HERE"));
+	
 	public static final Map<String, String> mapWidgetTypeToMidUrl = new HashMap<String, String>(Map.of(
 			"currentWeather", "&appid=",
 			"forecastWeather", "&appid=",
 			"calendar", "",
 			"stock", ""));
+	
 	public static final Map<String, String> mapWidgetTypeToApiKeyName = new HashMap<String, String>(Map.of(
 			"currentWeather", "weather_key",
 			"forecastWeather", "weather_key",
