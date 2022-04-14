87f7f63812ae6a00587e291ae4294fae7284cdc5
leoflewis <fleonardolewis@gmail.com>
2022-04-13 20:36:05 -0500

    finished weather display

--- frontend/499/src/app/home/WeatherDisplay.ts
+++ frontend/499/src/app/home/WeatherDisplay.ts
@@ -1,15 +1,18 @@
 export class WeatherDisplay {
     public name: string;
-    public temp: string;
+    public temp: number;
     public country: string;
-
-
+    public temp_min: number;
+    public temp_max: number;
+    public feels_like: number;
 
     public constructor(weather: any) {
         let data = JSON.parse(weather);
         this.name = data.name;
         this.temp = data.main.temp;
         this.country = data.sys.country
+        this.temp_max = data.main.temp_max;
+        this.temp_min = data.main.temp_min;
+        this.feels_like = data.main.feels_like;
     }
-   
-  }
+}
