b581ad9d76ad7ca848a71e53b09c0a7a8574fadc
Gilbert <jf3115ir@go.minnstate.edu>
2022-04-21 18:26:47 -0500

    Forecasts display correctly.

--- frontend/499/src/app/home/displays/ForecastDisplay.ts
+++ frontend/499/src/app/home/displays/ForecastDisplay.ts
@@ -7,15 +7,14 @@ export class ForecastDisplay {
 
     public constructor(forecast: any, widgetId: number) {
         let data = JSON.parse(forecast);
-        this.name = data.name;
-        this.country = data.sys.country;
+        this.name = data.city.name;
+        this.country = data.city.country;
         this.temps = [0,0,0]
         this.descrips = ['','','']
         this.widgetId = widgetId;
-        var i: number;
-        for (i = 0; i < 3; i++) {
+        for (let i = 0; i < 3; i++) {
             this.temps[i] = data.list[i].main.temp;
-            this.descrips[i] = data.list[i].main.description; 
+            this.descrips[i] = data.list[i].weather[0].description; 
         }
     }
 }
